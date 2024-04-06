4121e74f2e867b33cae74b71c1418d16b4cf4f3d
Barnie <bran.alexandru.2002@gmail.com>
2024-04-06 18:56:43 +0300

    implemented auto update app and endpoint statuses

--- watchdog/src/app.ts
+++ watchdog/src/app.ts
@@ -11,4 +11,164 @@ db.once('open', () => {
     console.info('Connected to MongoDB!');
 });
 
-updateAtInterval(10);
+Apps.create({
+    appName: 'Test App 1',
+    status: 'STABLE',
+    endpoints: [
+        {
+            endpointName: 'Get Posts 1',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            method: 'GET',
+            status: 'STABLE',
+            logs: [
+                {
+                    time: new Date(),
+                    response: 200
+                },
+                {
+                    time: new Date(),
+                    response: 302
+                }
+            ]
+        },
+        {
+            endpointName: 'Get Posts 2',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            method: 'GET',
+            status: 'STABLE',
+            logs: [
+                {
+                    time: new Date(),
+                    response: 200
+                },
+                {
+                    time: new Date(),
+                    response: 302
+                },
+                {
+                    time: new Date(),
+                    response: 400
+                },
+                {
+                    time: new Date(),
+                    response: 200
+                }
+            ]
+        }
+    ],
+    reports: [
+        {
+            endpointName: "Get Posts 1",
+            status: "UNSTABLE",
+            message: "Response code 400",
+            fixed: true
+        }
+    ]
+})
+
+Apps.create({
+    appName: 'Test App 2',
+    status: 'STABLE',
+    endpoints: [
+        {
+            endpointName: 'Get Posts 1',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            method: 'GET',
+            status: 'STABLE',
+            logs: [
+                {
+                    time: new Date(),
+                    response: 400
+                },
+                {
+                    time: new Date(),
+                    response: 302
+                }
+            ]
+        },
+        {
+            endpointName: 'Get Posts 2',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            method: 'GET',
+            status: 'STABLE',
+            logs: [
+                {
+                    time: new Date(),
+                    response: 200
+                },
+                {
+                    time: new Date(),
+                    response: 302
+                },
+                {
+                    time: new Date(),
+                    response: 400
+                },
+                {
+                    time: new Date(),
+                    response: 200
+                }
+            ]
+        }
+    ],
+    reports: []
+})
+
+Apps.create({
+    appName: 'Test App 3',
+    status: 'STABLE',
+    endpoints: [
+        {
+            endpointName: 'Get Posts 1',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            method: 'GET',
+            status: 'STABLE',
+            logs: [
+                {
+                    time: new Date(),
+                    response: 200
+                },
+                {
+                    time: new Date(),
+                    response: 302
+                }
+            ]
+        },
+        {
+            endpointName: 'Get Posts 2',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            method: 'GET',
+            status: 'STABLE',
+            logs: [
+                {
+                    time: new Date(),
+                    response: 200
+                },
+                {
+                    time: new Date(),
+                    response: 302
+                },
+                {
+                    time: new Date(),
+                    response: 200
+                }
+            ]
+        }
+    ],
+    reports: [
+        {
+            endpointName: "Get Posts 1",
+            status: "UNSTABLE",
+            message: "Response code 400",
+            fixed: true
+        },
+        {
+            endpointName: "Get Posts 2",
+            status: "UNSTABLE",
+            message: "Response code 400",
+            fixed: false
+        }
+    ]
+})
+
+updateAtInterval(5);
