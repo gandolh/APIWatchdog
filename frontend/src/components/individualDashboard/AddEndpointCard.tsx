22827c3656ba1d5f36239949522621f3e4c926b5
gandolh <29356778+gandolh@users.noreply.github.com>
2024-04-07 01:09:09 +0300

    create endpoint

---
+++ frontend/src/components/individualDashboard/AddEndpointCard.tsx
@@ -0,0 +1,90 @@
+import {
+  Button,
+  Card,
+  Divider,
+  Group,
+  Paper,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal } from "@mantine/core";
+import { useState } from "react";
+import { addEndpointToApp } from "../ApiCaller";
+
+interface newEndpoint {
+  name: string;
+}
+
+interface AddAppFormProps {
+  OnClose: () => void;
+  appId : string;
+}
+
+const AddEndpointForm = ({ OnClose, appId }: AddAppFormProps) => {
+  const [endpoint, setEndpoint] = useState<newEndpoint>({
+    name: "",
+  } as newEndpoint);
+
+  function updateName(newName: string) {
+    setEndpoint((prev) => ({ ...prev, name: newName }));
+  }
+
+  // function updateUrl(newUrl: string) {
+  //   setEndpoint((prev) => ({ ...prev, url: newUrl }));
+  // }
+
+  const HandleAddEndpoint = () => {
+    addEndpointToApp(appId,endpoint.name).then((el) => {
+      if (el === 0) OnClose();
+    });
+    // console.log(endpoint);
+  };
+
+  return (
+    <Stack>
+      <>
+        {/* <Paper withBorder p="sm"> */}
+          <TextInput
+            required
+            label="Endpoint url"
+            placeholder="https://www.google.com/"
+            value={endpoint.name}
+            onChange={(event) => updateName(event.currentTarget.value)}
+            radius="md"
+          />
+        {/* </Paper> */}
+      </>
+      <Button onClick={HandleAddEndpoint}> Add endpoint </Button>
+    </Stack>
+  );
+};
+
+interface AddEndpointCard {
+  OnRefetchData: () => void;
+  appId : string;
+}
+
+const AddEndpointCard = ({ OnRefetchData, appId }: AddEndpointCard) => {
+  const [opened, { open, close }] = useDisclosure(false);
+  function handleClose() {
+    close();
+    OnRefetchData();
+  }
+  return (
+    <>
+      <Modal opened={opened} onClose={close} title="Add endpoint" centered>
+        <AddEndpointForm OnClose={handleClose} appId={appId} />
+      </Modal>
+      <Card
+        onClick={open}
+        className="flex justify-center items-center fw-bold "
+      >
+        Create Endpoint
+      </Card>
+    </>
+  );
+};
+
+export default AddEndpointCard;
--- frontend/src/components/individualDashboard/EndpointDashboards.tsx
+++ frontend/src/components/individualDashboard/EndpointDashboards.tsx
@@ -1,19 +1,65 @@
 import { Card, Group, Stack } from "@mantine/core";
-import { EndpointData, Log } from "../publicDashboard/PubDashCard";
 import ColoredStatus, { getBgColor } from "../publicDashboard/ColoredStatus";
 import { Status } from "../../types/Status";
+import iEndpoint from "../../types/IEndpoint";
+import { useEffect, useState } from "react";
+import iLog from "../../types/ILog";
+import AddEndpointCard from "./AddEndpointCard";
 
 interface EndpointDashboardsProps {
-  endpoints: EndpointData[];
+  endpoints: iEndpoint[];
+  OnRefetchData : () => void;
+  appId : string;
 }
 
 interface EndpointCardProps {
-  name: string;
+  name: String;
   status: Status;
-  logs: Log[];
+  logs?: iLog[];
+}
+
+export interface MyLog {
+  time: Date;
+  status: Status;
+}
+
+export function getStatus(statusCode: Number): Status {
+  if (statusCode === 200 || statusCode === 201 || statusCode === 302)
+    return "Stable";
+  else return "Unstable";
 }
 
 const EndpointCard = ({ name, status, logs }: EndpointCardProps) => {
+  // ultimele 10 logs
+  // 200, 201, 302 - verde
+  // daca sunt toate 10 down -> down
+  // altfel unstable
+
+  const [last10Logs, setLast10Logs] = useState<MyLog[]>([]);
+
+  useEffect(() => {
+    // console.log(logs);
+    if (logs === undefined) return;
+    const newLogs = [...logs];
+    for (let i = 0; i < newLogs.length; i++)
+      newLogs[i].time = new Date(newLogs[i].time);
+
+    newLogs.sort(function (a, b) {
+      return b.time.getTime() - a.time.getTime();
+    });
+    newLogs.slice(0, 10);
+
+    const myLogs: MyLog[] = newLogs.map((el) => {
+      return { time: el.time, status: getStatus(el.response) };
+    });
+
+    const allDown = myLogs.every((el) => el.status === "Unstable");
+    if (allDown) myLogs.forEach((el) => (el.status = "Down"));
+
+    setLast10Logs(myLogs);
+  }, [logs]);
+
+  // console.log(last10Logs);
   return (
     <Card>
       <div>
@@ -22,10 +68,16 @@ const EndpointCard = ({ name, status, logs }: EndpointCardProps) => {
       </div>
       <div>
         <Group gap={0} grow h={30}>
-          {logs.map((log) => (
+          {last10Logs.map((log) => (
             <div className="h-full flex flex-col justify-center items-center">
-              <div> {log.timeStamp.getHours()}:{log.timeStamp.getMinutes() < 10 && "0"}{log.timeStamp.getMinutes()}</div>
-              <div className={"rounded-full w-6 h-6" + getBgColor(log.status)}></div>
+              <div>
+                {" "}
+                {log.time.getHours()}:{log.time.getMinutes() < 10 && "0"}
+                {log.time.getMinutes()}
+              </div>
+              <div
+                className={"rounded-full w-6 h-6" + getBgColor(log.status)}
+              ></div>
             </div>
           ))}
         </Group>
@@ -34,20 +86,18 @@ const EndpointCard = ({ name, status, logs }: EndpointCardProps) => {
   );
 };
 
-const EndpointsDashboard = ({ endpoints }: EndpointDashboardsProps) => {
-
+const EndpointsDashboard = ({ endpoints, appId,  OnRefetchData }: EndpointDashboardsProps) => {
 
   return (
     <>
       <Stack gap={10} className="w-full h-full">
+      <AddEndpointCard OnRefetchData={OnRefetchData} appId={appId}/>
         {endpoints.map((endpoint) => (
-          <Stack>
             <EndpointCard
-              name={endpoint.EndpointName}
-              status={endpoint.EndpointStatus}
-              logs={endpoint.Logs}
+              name={endpoint.name}
+              status={endpoint.status}
+              logs={endpoint.logs}
             />
-          </Stack>
         ))}
       </Stack>
     </>
